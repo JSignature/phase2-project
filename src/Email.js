5d2fea7d6e6fd635de27c8270299d28b45301de1
Jimmy Andreas <jimmysignature@gmail.com>
2022-07-03 16:57:32 -0400

    Styling on Email done

--- src/Email.js
+++ src/Email.js
@@ -1,8 +1,33 @@
-import { Container } from '@mui/system'
 import { React, useState, useEffect } from 'react'
+import Container from '@mui/material/Container'
+import Grid from '@mui/material/Grid'
+import Card from '@mui/material/Card'
+import CardHeader from '@mui/material/CardHeader'
+import CardMedia from '@mui/material/CardMedia'
+import CardContent from '@mui/material/CardContent'
+import CardActions from '@mui/material/CardActions'
+import Collapse from '@mui/material/Collapse'
+import Avatar from '@mui/material/Avatar'
+import IconButton from '@mui/material/IconButton'
+import Typography from '@mui/material/Typography'
+import { red } from '@mui/material/colors'
+import FavoriteIcon from '@mui/icons-material/Favorite'
+import ShareIcon from '@mui/icons-material/Share'
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore'
+import MoreVertIcon from '@mui/icons-material/MoreVert'
+import Button from '@mui/material/Button'
+import TextField from '@mui/material/TextField'
 import Box from '@mui/material/Box'
-
+import MenuItem from '@mui/material/MenuItem'
+import InputLabel from '@mui/material/InputLabel'
+import FormControl from '@mui/material/FormControl'
+import Select from '@mui/material/Select'
+import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns'
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider'
+import { DateTimePicker } from '@mui/x-date-pickers/DateTimePicker'
 import NavBar from './NavBar'
+import { dividerClasses } from '@mui/material'
+import Paper from '@mui/material/Paper'
 
 const Email = () => {
   const [clients, setClients] = useState([])
@@ -12,7 +37,7 @@ const Email = () => {
   const [templateProp, setTemplateProp] = useState([])
   const [clientsName, setClientsName] = useState('Please Select a Client')
   const [dogsName, setDogsName] = useState('')
-  const [date, setDate] = useState('Please Select a Date')
+  const [date, setDate] = useState(new Date())
 
   function handleClientChange(e) {
     setFilteredClient(e.target.value)
@@ -43,10 +68,6 @@ const Email = () => {
     setTemplateProp(templatePropStr)
   }, [filteredTemplate])
 
-  function handleDateChange(e) {
-    setDate(e.target.value)
-  }
-
   useEffect(() => {
     fetch('http://localhost:4000/clients')
       .then(resp => resp.json())
@@ -61,40 +82,101 @@ const Email = () => {
   return (
     <div>
       <NavBar />
-      <Box
-        sx={{
-          height: '40vh',
-          width: '50vh',
-          border: '1px solid',
-          textAlign: 'center',
-          margin: 'auto',
-        }}
-      >
-        <h1>Email</h1>
-        <select name="clientFilter" id="" onChange={handleClientChange}>
-          <option value="">Select a client</option>
+      <h1>Email</h1>
+      <Container>
+        <Grid container spacing={2}>
+          <Grid item xs={12}>
+            <FormControl fullWidth>
+              <InputLabel>Client's Name</InputLabel>
+              <Select
+                label="Client's Name"
+                value={filteredClient}
+                onChange={handleClientChange}
+              >
+                {clients.map(client => (
+                  <MenuItem key={client.id} value={client.clientName}>
+                    {client.clientName}
+                  </MenuItem>
+                ))}
+              </Select>
+            </FormControl>
+          </Grid>
 
-          {clients.map(client => (
-            <option key={client.id} value={client.clientName}>
-              {client.clientName}
-            </option>
-          ))}
-        </select>{' '}
-        <br />
-        <input type="date" name="Date" id="" onChange={handleDateChange} />{' '}
-        <br />
-        <select name="templateFilter" id="" onChange={handleTemplateChange}>
-          <option value="">Select a template</option>
-          {templates.map(template => (
-            <option key={template.id} value={template.tempName}>
-              {template.tempName}
-            </option>
-          ))}
-        </select>
-        <p>
-          {templateProp.length === 0 ? ' ' : eval('`' + templateProp + '`')}
-        </p>
-      </Box>
+          <Grid item xs={12}>
+            <LocalizationProvider dateAdapter={AdapterDateFns}>
+              <DateTimePicker
+                renderInput={props => <TextField {...props} />}
+                label="DateTimePicker"
+                value={date}
+                onChange={newValue => {
+                  setDate(newValue)
+                }}
+              />
+            </LocalizationProvider>
+          </Grid>
+          <Grid item xs={12}>
+            <FormControl fullWidth>
+              <InputLabel>Template</InputLabel>
+              <Select
+                id="demo-simple-select"
+                value={filteredTemplate}
+                label="Template"
+                onChange={handleTemplateChange}
+              >
+                {templates.map(template => (
+                  <MenuItem key={template.id} value={template.tempName}>
+                    {template.tempName}
+                  </MenuItem>
+                ))}
+              </Select>
+            </FormControl>
+          </Grid>
+
+          <Grid item xs={12}>
+            <Box
+              sx={{
+                '& .MuiTextField-root': { width: 1 },
+              }}
+            >
+              {templateProp.length === 0 ? (
+                ' '
+              ) : (
+                <TextField
+                  name="Final Email"
+                  multiline
+                  minRows={1}
+                  maxRows={100}
+                  label="Email"
+                  value={eval('`' + templateProp + '`')}
+                  focused
+                />
+              )}
+            </Box>
+
+            {/* <Box
+              sx={{
+                display: 'flex',
+                flexWrap: 'wrap',
+                '& > :not(style)': {
+                  m: 1,
+                  width: 'auto',
+                  height: 'auto',
+                  
+                },
+              }}
+            >
+              <Paper elevation={5}>
+                {templateProp.length === 0
+                  ? ' '
+                  : eval('`' + templateProp + '`')}{' '}
+              </Paper>
+            </Box> */}
+            {/* <p>
+              {templateProp.length === 0 ? ' ' : eval('`' + templateProp + '`')}
+            </p> */}
+          </Grid>
+        </Grid>
+      </Container>
     </div>
   )
 }
